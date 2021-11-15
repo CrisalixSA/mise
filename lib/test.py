08eeae2fb68ff28ce4b73b597b91b865c1eff5ea
gilTriginer <gil.triginer@gmail.com>
2021-11-15 15:26:13 +0100

    improve test

--- lib/test.py
+++ lib/test.py
@@ -1,25 +1,75 @@
 import numpy as np
-from mise import MISE
+import torch
 import time
+import os
+import trimesh
+import math
+import argparse
 
-t0 = time.time()
-extractor = MISE(1, 2, 0.)
+from skimage.measure import marching_cubes
 
-p = extractor.query()
-i = 0
+from mise import MISE
 
-while p.shape[0] != 0:
-    print(i)
-    print(p)
-    v = 2 * (p.sum(axis=-1) > 2).astype(np.float64) - 1
-    extractor.update(p, v)
+def sphere_sdf(x, radius=0.5):
+    return torch.linalg.norm(x, axis=-1) - radius
+
+def run_mise(sdf, resolution_0, depth, threshold, device):
+
+    extractor = MISE(resolution_0, depth, threshold)
+
     p = extractor.query()
-    i += 1
-    if (i >= 8):
-        break
+    n_iterations = 0
+    max_iterations = 8
 
-print(extractor.to_dense())
-# p, v = extractor.get_points()
-# print(p)
-# print(v)
-print('Total time: %f' % (time.time() - t0))
+    while p.shape[0] != 0:
+
+        print(f'n_iterations: {n_iterations}')
+        print(f'number of extractor queries: {len(p)}')
+
+        pointsf = (p / extractor.resolution) - 0.5
+        pointsf = torch.FloatTensor(pointsf, device=device)
+
+        v = sdf(pointsf).detach().numpy().astype(np.float64)
+        extractor.update(p, v)
+        p = extractor.query()
+        n_iterations += 1
+        if (n_iterations >= max_iterations):
+            break
+    return extractor
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--output-mesh',
+        default=None,
+        help='Output mesh path. If not provided, no output is saved.'
+    )
+    args = parser.parse_args()
+    resolution_0 = 32 # Resolution in the first iteration
+    depth = 4   # Number of subdivisions of the initial resolution. 
+                # Final resolution = 2^(log2(resolution_0)*depth)
+    threshold = 1e-5 # value of the occupancy that marks surface 
+
+    print(
+        f'Initial resolution: {resolution_0} \n'
+        f'Number of subdivisions: {depth} \n'
+        f'Final resolution: {2**(math.log2(resolution_0) + depth)}'
+        )
+
+    device = torch.device('cpu') # TODO: cuda_if_available
+
+    t0 = time.time()
+    extractor = run_mise(sphere_sdf, resolution_0, depth, threshold, device)
+    print('Time to sparse values: %f' % (time.time() - t0))
+    dense_values = extractor.to_dense()
+    print('Time to dense values: %f' % (time.time() - t0))
+    vertices, faces, normals, values = marching_cubes(volume=dense_values, level=0)
+    print('Time to mesh: %f' % (time.time() - t0))
+
+    if args.output_mesh:
+        mesh = trimesh.Trimesh(vertices=vertices, faces=faces)
+        mesh.export(args.output_mesh)
+
+if __name__ == '__main__':
+    main()
