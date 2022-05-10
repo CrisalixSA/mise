0e046eb9697133fd3bc2dcbfb627db7561610992
Antonio Canela <antonio.canela@crisalix.com>
2022-05-10 11:19:29 +0200

    Updated setup py

--- setup.py
+++ setup.py
@@ -1,26 +1,31 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+import os
+from setuptools import setup, find_packages
 from distutils.extension import Extension
+from pkg_resources import parse_requirements
 from Cython.Build import cythonize
 
-# Extensions
-# mise (efficient mesh extraction)
-mise_module = Extension(
-    'mise',
-    sources=[
-        'lib/mise.pyx'
-    ],
-)
+
+PACKAGE_NAME = 'mise'
+VERSION = '0.1'
+REQUIREMENTS_FILE_PATH = 'requirements.txt'
 
 # Gather all extension modules
 ext_modules = [
-    mise_module
+    Extension(
+        'mise',
+        sources=[
+            'lib/mise.pyx'
+        ],
+    )
 ]
 
+# Build setup
 setup(
-    name='mise',
-    version='0.1',
-    ext_modules=cythonize(ext_modules)
+    name=PACKAGE_NAME,
+    version=VERSION,
+    install_requires=[
+        req.__str__()
+        for req in parse_requirements(open(REQUIREMENTS_FILE_PATH).read())
+    ],
+    ext_modules=cythonize(ext_modules),
 )
